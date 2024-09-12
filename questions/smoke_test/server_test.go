1735f2bdcb21cbd987ac7a669dc77e86533229bf
AVAniketh0905 <adimoolamaniketh@gmail.com>
2024-09-12 18:47:56 +0530

    smoketest test updt

--- questions/smoke_test/server_test.go
+++ questions/smoke_test/server_test.go
@@ -3,6 +3,7 @@ package smoketest
 import (
 	"fmt"
 	"net"
+	"reflect"
 	"testing"
 
 	"github.com/AVAniketh0905/protohackers/internal"
@@ -21,12 +22,22 @@ func TestSmokeTest(t *testing.T) {
 	defer client.Close()
 	fmt.Println("client started...")
 
-	for i := range 7 {
-		// time.Sleep(time.Duration((i + 1)) * time.Second)
-		msg := []byte(fmt.Sprintf("Hello, %d ", i))
-		_, err = client.Write(msg)
+	msgs := []string{"Hello1", "Hello2", "Hello3", "Hello4", "Hello5"}
+
+	for i := range 5 {
+		_, err = client.Write([]byte(msgs[i]))
 		if err != nil {
 			t.Error(err)
+		}
+
+		buf := make([]byte, 1024)
+		n, err := client.Read(buf)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if !reflect.DeepEqual(string(buf[:n]), msgs[i]) {
+			t.Errorf("msg doesnt match, actual %v, got %v", msgs[i], string(buf[:n]))
 		}
 	}
 }
