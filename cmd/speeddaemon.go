c565e1f5413188beb991ab80d96f22c3fb8dedd8
AVAniketh0905 <adimoolamaniketh@gmail.com>
2024-10-01 18:03:59 +0530

    speed daemon all msg types

--- cmd/speeddaemon.go
+++ cmd/speeddaemon.go
@@ -47,9 +47,13 @@ func (s Str) Marshall() (b []byte, err error) {
 type MsgType uint8
 
 const (
-	ErrorType  MsgType = 16 //0x10
-	PlateType  MsgType = 32 //0x20
-	TicketType MsgType = 33 //0x21
+	ErrorType         MsgType = 16  //0x10
+	PlateType         MsgType = 32  //0x20
+	TicketType        MsgType = 33  //0x21
+	WantHeartBeatType MsgType = 64  //0x40
+	HeartBeatType     MsgType = 65  //0x41
+	IAmCameraType     MsgType = 128 //0x80
+	IAmDispatcherType MsgType = 129 //0x81
 )
 
 type Message interface {
@@ -80,7 +84,7 @@ func (e *Error) Unmarshall(data []byte) error {
 
 	msgType := MsgType(msgTypebyte)
 	if msgType != e.Type() {
-		return fmt.Errorf("mismtach Msg Type")
+		return fmt.Errorf("mismatch Msg Type")
 	}
 
 	rmgBytes := make([]byte, 1024)
@@ -131,7 +135,7 @@ func (p *Plate) Unmarshall(data []byte) error {
 
 	msgType := MsgType(msgTypebyte)
 	if msgType != p.Type() {
-		return fmt.Errorf("mismtach Msg Type")
+		return fmt.Errorf("mismatch Msg Type")
 	}
 
 	rmgBytes := make([]byte, 1024)
@@ -159,7 +163,7 @@ func (p Plate) Marshall() (data []byte, err error) {
 		return nil, err
 	}
 	data = append(data, plateStr...)
-	binary.BigEndian.AppendUint32(data, p.Timestamp)
+	binary.BigEndian.PutUint32(data, p.Timestamp)
 
 	return data, nil
 }
@@ -192,7 +196,7 @@ func (t *Ticket) Unmarshall(data []byte) error {
 
 	msgType := MsgType(msgTypebyte)
 	if msgType != t.Type() {
-		return fmt.Errorf("mismtach Msg Type")
+		return fmt.Errorf("mismatch Msg Type")
 	}
 
 	rmgBytes := make([]byte, 1024)
@@ -230,13 +234,167 @@ func (t *Ticket) Unmarshall(data []byte) error {
 }
 
 func (t Ticket) Marshall() (data []byte, err error) {
+	data = append(data, byte(t.Type()))
+	plateStr, err := t.Plate.Marshall()
+	if err != nil {
+		return nil, err
+	}
+	data = append(data, plateStr...)
+	binary.BigEndian.PutUint16(data, t.Road)
+	binary.BigEndian.PutUint16(data, t.Mile1)
+	binary.BigEndian.PutUint32(data, t.Timestamp1)
+	binary.BigEndian.PutUint16(data, t.Mile2)
+	binary.BigEndian.PutUint32(data, t.Timestamp2)
+	binary.BigEndian.PutUint16(data, t.Speed)
+	return data, nil
+}
+
+type WantHeartBeat struct {
+	Message
+	Interval uint32
+}
+
+func (hb WantHeartBeat) Type() MsgType {
+	return WantHeartBeatType
+}
+
+func (whb *WantHeartBeat) Unmarshall(data []byte) error {
+	r := bytes.NewReader(data)
+	msgTypebyte, err := r.ReadByte()
+	if err != nil {
+		return err
+	}
+
+	msgType := MsgType(msgTypebyte)
+	if msgType != whb.Type() {
+		return fmt.Errorf("mismatch Msg Type")
+	}
+
+	rmgBytes := make([]byte, 1024)
+	n, err := r.Read(rmgBytes)
+	if err != nil {
+		return err
+	}
+
+	whb.Interval = binary.BigEndian.Uint32(rmgBytes[:n])
+	return nil
+}
+
+func (whb WantHeartBeat) Marshall() (data []byte, err error) {
+	data = append(data, byte(whb.Type()))
+	binary.BigEndian.PutUint32(data, whb.Interval)
+	return data, nil
+}
+
+type HeartBeat struct {
+}
+
+func (hb HeartBeat) Type() MsgType {
+	return HeartBeatType
+}
+
+type IAmCamera struct {
+	Message
+	Road  uint16
+	Mile  uint16
+	Limit uint16 // miles per hour
+}
+
+func (c IAmCamera) Type() MsgType {
+	return IAmCameraType
+}
+
+func (c *IAmCamera) Unmarshall(data []byte) error {
+	r := bytes.NewReader(data)
+	msgTypebyte, err := r.ReadByte()
+	if err != nil {
+		return err
+	}
+
+	msgType := MsgType(msgTypebyte)
+	if msgType != c.Type() {
+		return fmt.Errorf("mismatch Msg Type")
+	}
+
+	rmgBytes := make([]byte, 1024)
+	n, err := r.Read(rmgBytes)
+	if err != nil {
+		return err
+	}
+	m := 0
+
+	c.Road = binary.BigEndian.Uint16(rmgBytes[m : m+4])
+	m += 4
+
+	c.Mile = binary.BigEndian.Uint16(rmgBytes[m : m+4])
+	m += 4
+
+	c.Limit = binary.BigEndian.Uint16(rmgBytes[m:n])
+
+	return nil
+}
+
+func (c IAmCamera) Marshall() (data []byte, err error) {
+	data = append(data, byte(c.Type()))
+	binary.BigEndian.PutUint16(data, c.Road)
+	binary.BigEndian.PutUint16(data, c.Mile)
+	binary.BigEndian.PutUint16(data, c.Limit)
+	return data, nil
+}
+
+type IAmDispatcher struct {
+	Message
+	NumRoads uint8
+	Roads    []uint16
+}
+
+func (d IAmDispatcher) Type() MsgType {
+	return IAmDispatcherType
+}
+
+func (d *IAmDispatcher) Unmarshall(data []byte) error {
+	r := bytes.NewReader(data)
+	msgTypebyte, err := r.ReadByte()
+	if err != nil {
+		return err
+	}
+
+	msgType := MsgType(msgTypebyte)
+	if msgType != d.Type() {
+		return fmt.Errorf("mismtach Msg Type")
+	}
+
+	rmgBytes := make([]byte, 1024)
+	_, err = r.Read(rmgBytes)
+	if err != nil {
+		return err
+	}
+	m := 0
+
+	d.NumRoads = uint8(rmgBytes[1])
+	m += 1
+
+	for range d.NumRoads {
+		road := binary.BigEndian.Uint16(rmgBytes[m : m+4])
+		d.Roads = append(d.Roads, road)
+		m += 4
+	}
+
+	return nil
+}
+
+func (d IAmDispatcher) Marshall() (data []byte, err error) {
+	data = append(data, byte(d.Type()))
+	data = append(data, byte(d.NumRoads))
+	for i := range d.NumRoads {
+		binary.BigEndian.PutUint16(data, d.Roads[i])
+	}
 	return data, nil
 }
 
 func (sd SpeedDeamon) Setup() context.Context { return context.TODO() }
 
 func (sd SpeedDeamon) Handler(ctx context.Context, conn net.Conn) {
-
 }
 
 func Run() {
