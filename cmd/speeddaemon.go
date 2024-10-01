e23f71adfea2bdfccc0b39a988a31f482017559e
AVAniketh0905 <adimoolamaniketh@gmail.com>
2024-10-01 10:15:56 +0530

    speed daemon creating types

--- cmd/speeddaemon.go
+++ cmd/speeddaemon.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"bytes"
 	"context"
+	"encoding/binary"
+	"fmt"
 	"net"
 
 	"github.com/AVAniketh0905/protohackers/internal"
@@ -40,6 +42,195 @@ func (s Str) Marshall() (b []byte, err error) {
 
 	b = append(b, s.Msg...)
 	return b, err
+}
+
+type MsgType uint8
+
+const (
+	ErrorType  MsgType = 16 //0x10
+	PlateType  MsgType = 32 //0x20
+	TicketType MsgType = 33 //0x21
+)
+
+type Message interface {
+	Type() MsgType
+	Unmarshall(data []byte) error
+	Marshall() ([]byte, error)
+}
+
+type Error struct {
+	Message
+	err Str
+}
+
+func (e Error) Type() MsgType {
+	return ErrorType
+}
+
+func (e Error) ErrMsg() string {
+	return string(e.err.Msg)
+}
+
+func (e *Error) Unmarshall(data []byte) error {
+	r := bytes.NewReader(data)
+	msgTypebyte, err := r.ReadByte()
+	if err != nil {
+		return err
+	}
+
+	msgType := MsgType(msgTypebyte)
+	if msgType != e.Type() {
+		return fmt.Errorf("mismtach Msg Type")
+	}
+
+	rmgBytes := make([]byte, 1024)
+	n, err := r.Read(rmgBytes)
+	if err != nil {
+		return err
+	}
+
+	var msg Str
+	if err := msg.Unmarshall(rmgBytes[:n]); err != nil {
+		return err
+	}
+
+	e.err = msg
+	return nil
+}
+
+func (e Error) Marshall() (p []byte, err error) {
+	p = append(p, byte(e.Type())) // type
+	msgStr, err := e.err.Marshall()
+	if err != nil {
+		return nil, err
+	}
+	p = append(p, msgStr...)
+	return p, nil
+}
+
+type Plate struct {
+	Message
+	Plate     Str
+	Timestamp uint32
+}
+
+func (p Plate) Type() MsgType {
+	return PlateType
+}
+
+func (p Plate) PlateMsg() string {
+	return string(p.Plate.Msg)
+}
+
+func (p *Plate) Unmarshall(data []byte) error {
+	r := bytes.NewReader(data)
+	msgTypebyte, err := r.ReadByte()
+	if err != nil {
+		return err
+	}
+
+	msgType := MsgType(msgTypebyte)
+	if msgType != p.Type() {
+		return fmt.Errorf("mismtach Msg Type")
+	}
+
+	rmgBytes := make([]byte, 1024)
+	n, err := r.Read(rmgBytes)
+	if err != nil {
+		return err
+	}
+
+	var plate Str
+	if err := plate.Unmarshall(rmgBytes[:n]); err != nil {
+		return err
+	}
+
+	timestamp := binary.BigEndian.Uint32(rmgBytes[plate.Len+1 : n])
+
+	p.Plate = plate
+	p.Timestamp = timestamp
+	return nil
+}
+
+func (p Plate) Marshall() (data []byte, err error) {
+	data = append(data, byte(p.Type()))
+	plateStr, err := p.Plate.Marshall()
+	if err != nil {
+		return nil, err
+	}
+	data = append(data, plateStr...)
+	binary.BigEndian.AppendUint32(data, p.Timestamp)
+
+	return data, nil
+}
+
+type Ticket struct {
+	Message
+	Plate      Str
+	Road       uint16
+	Mile1      uint16
+	Timestamp1 uint32
+	Mile2      uint16
+	Timestamp2 uint32
+	Speed      uint16 // 100x miles per hour
+}
+
+func (t Ticket) Type() MsgType {
+	return TicketType
+}
+
+func (t Ticket) PlateMsg() string {
+	return string(t.Plate.Msg)
+}
+
+func (t *Ticket) Unmarshall(data []byte) error {
+	r := bytes.NewReader(data)
+	msgTypebyte, err := r.ReadByte()
+	if err != nil {
+		return err
+	}
+
+	msgType := MsgType(msgTypebyte)
+	if msgType != t.Type() {
+		return fmt.Errorf("mismtach Msg Type")
+	}
+
+	rmgBytes := make([]byte, 1024)
+	n, err := r.Read(rmgBytes)
+	if err != nil {
+		return err
+	}
+	m := 0
+
+	var plate Str
+	if err := plate.Unmarshall(rmgBytes[:n]); err != nil {
+		return err
+	}
+	t.Plate = plate
+	m += int(plate.Len) + 1
+
+	t.Road = binary.BigEndian.Uint16(rmgBytes[m : m+2])
+	m += 2
+
+	t.Mile1 = binary.BigEndian.Uint16(rmgBytes[m : m+2])
+	m += 2
+
+	t.Timestamp1 = binary.BigEndian.Uint32(rmgBytes[m : m+4])
+	m += 4
+
+	t.Mile2 = binary.BigEndian.Uint16(rmgBytes[m : m+2])
+	m += 2
+
+	t.Timestamp2 = binary.BigEndian.Uint32(rmgBytes[m : m+4])
+	m += 4
+
+	t.Speed = binary.BigEndian.Uint16(rmgBytes[m:n])
+
+	return nil
+}
+
+func (t Ticket) Marshall() (data []byte, err error) {
+	return data, nil
 }
 
 func (sd SpeedDeamon) Setup() context.Context { return context.TODO() }
--- tests/speeddaemon_test.go
+++ tests/speeddaemon_test.go
@@ -29,5 +29,103 @@ func TestStr(t *testing.T) {
 			t.Errorf("i: %d h: %q, msgs doesnt match, expected %q, got %q", i, h, msg[i], string(dataStr.Msg))
 		}
 	}
+}
+
+func TestMsgTypes(t *testing.T) {
+	hexMsgs := []string{"1003626164", "100b696c6c6567616c206d7367"}
+	errMsgs := []string{"bad", "illegal msg"}
+
+	for i, hm := range hexMsgs {
+		hexBytes, err := hex.DecodeString(hm)
+		if err != nil {
+			t.Error(err)
+		}
+		var errMsg cmd.Error
+		err = errMsg.Unmarshall(hexBytes)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if !reflect.DeepEqual(errMsg.ErrMsg(), errMsgs[i]) {
+			t.Errorf("msgs did not match, expected %q, got %q", errMsgs[i], errMsg.ErrMsg())
+		}
+	}
+
+	hexMsgs = []string{"2004554e3158000003e8", "200752453035424b470001e240"}
+	plateMsgs := []struct {
+		plate     string
+		timestamp uint32
+	}{
+		{
+			plate:     "UN1X",
+			timestamp: 1000,
+		},
+		{
+			plate:     "RE05BKG",
+			timestamp: 123456,
+		},
+	}
+
+	for i, hm := range hexMsgs {
+		hexBytes, err := hex.DecodeString(hm)
+		if err != nil {
+			t.Error(err)
+		}
+
+		var plateMsg cmd.Plate
+		err = plateMsg.Unmarshall(hexBytes)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if plateMsg.Type() != cmd.PlateType {
+			t.Error("mismatch Msg Type")
+		}
+
+		if !reflect.DeepEqual(plateMsg.PlateMsg(), plateMsgs[i].plate) {
+			t.Errorf("msgs did not match, expected %q, got %q", plateMsgs[i].plate, plateMsg.PlateMsg())
+		}
+
+		if !reflect.DeepEqual(plateMsg.Timestamp, plateMsgs[i].timestamp) {
+			t.Errorf("msgs did not match, expected %v, got %v", plateMsgs[i].timestamp, plateMsg.Timestamp)
+		}
+	}
+
+	hexMsgs = []string{"2104554e3158007b00080000000000090000002d1f40"}
+	ticketMsgs := []struct {
+		plate      string
+		road       uint16
+		mile1      uint16
+		timestamp1 uint32
+		mile2      uint16
+		timestamp2 uint32
+		speed      uint16
+	}{
+		{plate: "UN1X", road: 123, mile1: 8, timestamp1: 0, mile2: 9, timestamp2: 45, speed: 8000},
+	}
+
+	for i, hm := range hexMsgs {
+		hexBytes, err := hex.DecodeString(hm)
+		if err != nil {
+			t.Error(err)
+		}
+
+		var ticket cmd.Ticket
+		if err := ticket.Unmarshall(hexBytes); err != nil {
+			t.Error(err)
+		}
+
+		if ticket.Type() != cmd.TicketType {
+			t.Error("mismatch Msg Type")
+		}
+
+		if !reflect.DeepEqual(ticketMsgs[i].plate, ticket.PlateMsg()) {
+			t.Errorf("msgs did not match, expected %v, got %v", ticketMsgs[i].plate, ticket.PlateMsg())
+		}
+
+		if !reflect.DeepEqual(ticketMsgs[i].speed, ticket.Speed) {
+			t.Errorf("msgs did not match, expected %v, got %v", ticketMsgs[i].speed, ticket.Speed)
+		}
+	}
 
 }
